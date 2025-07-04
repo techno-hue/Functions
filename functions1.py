a64e1fd661e2ff90d4c617616098cd0fbca8123c
techno-hue <rishikarajak.0287@gmail.com>
2025-07-04 09:19:38 +0530

    Add files via upload

---
+++ functions1.py
@@ -0,0 +1,20 @@
+a = 'python'
+print(a.upper())
+print(a.capitalize())
+print(a.title())
+print(a.swapcase())
+print(a.islower())
+print(a.isupper())
+print(a.istitle())
+print(a.isalpha())
+print(a.isalnum())
+print(a.isdigit())
+print(a.isspace())
+print(a.strip())
+print(a.rstrip())
+print(a.split())
+print(a.zfill())
+
+
+
+
