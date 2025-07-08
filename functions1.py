29f0786f160fdfe16ca1c7daad68cd2912ac60ff
techno-hue <rishikarajak.0287@gmail.com>
2025-07-08 11:43:41 +0530

    Add files via upload

--- functions1.py
+++ functions1.py
@@ -1,19 +1,55 @@
 a = 'python'
+def excapital(a):
+    a.upper(a)
 print(a.upper())
+def exuppercase(a):
+    a.capitalize(a)
 print(a.capitalize())
+def extittling(a):
+    a.title(a)
 print(a.title())
+def exswapping(a):
+    a.swapcase(a)
 print(a.swapcase())
+def exlowercase(a):
+    a.islower()
 print(a.islower())
+def exupprrcase(a):
+    a.isupper()
 print(a.isupper())
+def extitlement(a):
+    a.istitle()
 print(a.istitle())
+def exalphabet(a):
+    a.isalpha(a)
 print(a.isalpha())
+def exnumber(a):
+    a.isalnum(a)
 print(a.isalnum())
+def exdigits(a):
+    a.isdigit(a)
 print(a.isdigit())
+def exspace(a):
+    a.isspace(a)
 print(a.isspace())
+def exstrips(a):
+    a.strip(a)
 print(a.strip())
+def exrstrips(a):
+    a.rstrip(a)
 print(a.rstrip())
+def exsplits(a):
+    a.split(a)
 print(a.split())
-print(a.zfill())
+
+
+
+
+
+
+
+
+
 
 
 
