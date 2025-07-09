f0d010e9ca8d2313c62afe1197e962b213a7fa0f
techno-hue <rishikarajak.0287@gmail.com>
2025-07-09 09:42:53 +0530

    Add files via upload

---
+++ functions3(TUPLE).py
@@ -0,0 +1,44 @@
+colours = ('Black','Brown','Purple','Blue','Green','Yellow','Orange','Red')
+print(colours)
+#Unpacking of tuple
+col1,col2,col3,col4,col5,col6,col7,col8=colours
+print(col1)
+print(col2)
+# Tuple Indexing
+print(colours[2])
+#Tuple Slicing
+print(colours[0:4])
+print(colours[::2])
+#Library function
+color=('Red','Orange','Yellow','Green','Blue','Purple','Brown','Black',)
+def length(color):
+     len(color)
+print(len(color))
+colour = ('Red','Orange','Yellow','Green','Blue','Purple','Brown','Black',)
+count = 0
+def counting (colour):
+    count(colour)
+print(colour(counting))
+animal=('dog','cat','sheep','rabbit',)
+index = 0 
+def EXindexing(animal):
+     index(animal)
+print(EXindexing(animal))
+parts = ('eyes','nose','hands', 'legs','brain','arms')
+def sorting (parts):
+    sorted(parts)
+print(sorting(parts))
+num1 = ('10','20','30','40','50')
+def maximum(num1):
+    max(num1)
+maximum(num1)
+def minimum(num1):
+    min(num1)
+minimum(num1)
+
+
+
+
+
+
+
